7bd5646a2fa0ad65c5d19c6e2044f1319b5138d2
bluepokeboy <>
2021-03-14 17:17:32 +0530

    Training Code added

--- src/main.py
+++ src/main.py
@@ -13,11 +13,26 @@ if __name__=='__main__':
     parser.add_argument('--x_path', type=str, default='../data/DIV2K_train_LR_bicubic/X2/')
     parser.add_argument('--y_path', type=str, default='../data/DIV2K_train_HR/')
     parser.add_argument('--scale_factor', type=int, default=2)
+    parser.add_argument('--model_save_path', type=str, default='./models')
 
     # training settings
     parser.add_argument('--total_step', type=int, default=200000)
     parser.add_argument('--batch_size', type=int, default=2)
     parser.add_argument('--num_workers', type=int, default=4)
+    parser.add_argument('--num_blocks', type=int, default=11)
+    parser.add_argument('--num_channels', type=int, default=18)
+    parser.add_argument('--conv_dim', type=int, default=128)
+    parser.add_argument('--scale_factor', type=int, default=2)
+    parser.add_argument('--lr', type=float, default=0.001)
+    parser.add_argument('--beta1', type=float, default=0.5)
+    parser.add_argument('--beta2', type=float, default=0.999)
+    parser.add_argument('--total_step', type=int, default=200000)
+    parser.add_argument('--trained_model', type=int, default=None)
+
+    #misc
+    parser.add_argument('--log_step', type=int, default=10)
+    parser.add_argument('--sample_step', type=int, default=100)
+    parser.add_argument('--model_save_step', type=int, default=1000)
 
     # config
     config = parser.parse_args()
@@ -37,3 +52,5 @@ if __name__=='__main__':
                                   batch_size=config.batch_size,
                                   shuffle=True,
                                   num_workers=config.num_workers)
+    training = Train(data_loader, config)
+    training.train()
---
+++ src/train.py
@@ -0,0 +1,139 @@
+import torch
+import torch.nn as nn
+import os
+from torchvision.utils import save_image, make_grid
+from model import SRMD
+import numpy as np
+
+torch.set_default_tensor_type(torch.DoubleTensor)
+
+class Train(object):
+    def __init__(self, data_loader, config):
+        # Data loader
+        self.data_loader = data_loader
+
+        # Model hyper-parameters
+        self.num_blocks = config.num_blocks
+        self.num_channels = config.num_channels
+        self.conv_dim = config.conv_dim
+        self.scale_factor = config.scale_factor
+
+        # Training settings
+        self.total_step = config.total_step
+        self.lr = config.lr
+        self.beta1 = config.beta1
+        self.beta2 = config.beta2
+        self.trained_model = config.trained_model
+        # self.use_tensorboard = config.use_tensorboard
+
+        # Path and step size
+        # self.log_path = config.log_path
+        # self.result_path = config.result_path
+        self.model_save_path = config.model_save_path
+        self.log_step = config.log_step
+        self.sample_step = config.sample_step
+        self.model_save_step = config.model_save_step
+
+        # Device configuration
+        self.device = config.device
+
+        self.build_model()
+        # if self.use_tensorboard:
+        #     self.build_tensorboard()
+
+        # Start with trained model
+        # if self.trained_model:
+        #     self.load_trained_model()
+
+    def build_model(self):
+        # model and optimizer
+        self.model = SRMD(self.num_blocks, self.num_channels, self.conv_dim, self.scale_factor)
+        self.optimizer = torch.optim.Adam(self.model.parameters(), self.lr, [self.beta1, self.beta2])
+
+        self.model.to(self.device)
+
+    def load_trained_model(self):
+        self.load(os.path.join(
+            self.model_save_path, '{}.pth'.format(self.trained_model)))
+        print('loaded trained models (step: {})..!'.format(self.trained_model))
+
+    def load(self, filename):
+        S = torch.load(filename)
+        self.model.load_state_dict(S['SR'])
+
+    # def build_tensorboard(self):
+    #     from logger import Logger
+    #     self.logger = Logger(self.log_path)
+
+    def update_lr(self, lr):
+        for param_group in self.optimizer.param_groups:
+            param_group['lr'] = lr
+
+    def reset_grad(self):
+        self.optimizer.zero_grad()
+
+    def detach(self, x):
+        return x.data
+
+    def train(self):
+        self.model.train()
+
+        # Reconst loss
+        reconst_loss = nn.MSELoss()
+
+        # Data iter
+        data_iter = iter(self.data_loader)
+        iter_per_epoch = len(self.data_loader)
+
+        # Start with trained model
+        if self.trained_model:
+            start = self.trained_model + 1
+        else:
+            start = 0
+
+        for step in range(start, self.total_step):
+            # Reset data_iter for each epoch
+            if (step+1) % iter_per_epoch == 0:
+                data_iter = iter(self.data_loader)
+
+            x, y = next(data_iter)
+            x, y = x.to(self.device), y.to(self.device)
+            y = y.to(torch.float64)
+
+            out = self.model(x)
+            loss = reconst_loss(out, y)
+
+            self.reset_grad()
+
+            # For decoder
+            loss.backward(retain_graph=True)
+
+            self.optimizer.step()
+
+            # Print out log info
+            if (step+1) % self.log_step == 0:
+                print("[{}/{}] loss: {:.4f}".format(step+1, self.total_step, loss.item()))
+
+            # Sample images
+            if (step+1) % self.sample_step == 0:
+                self.model.eval()
+                reconst = self.model(x)
+
+                def to_np(x):
+                    return x.data.cpu().numpy()
+
+                tmp = nn.Upsample(scale_factor=self.scale_factor)(x.data[:,0:3,:])
+                pairs = torch.cat((tmp.data[0:2,:], reconst.data[0:2,:], y.data[0:2,:]), dim=3)
+                grid = make_grid(pairs, 2)
+                from PIL import Image
+                tmp = np.squeeze(grid.numpy().transpose((1, 2, 0)))
+                tmp = (255 * tmp).astype(np.uint8)
+                Image.fromarray(tmp).save('./samples/test_%d.jpg' % (step + 1))
+
+            # Save check points
+            if (step+1) % self.model_save_step == 0:
+                self.save(os.path.join(self.model_save_path, '{}.pth'.format(self.trained_model)))
+
+    def save(self, filename):
+        model = self.model.state_dict()
+        torch.save({'SR': model}, filename)
