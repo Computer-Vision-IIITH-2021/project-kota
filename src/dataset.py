859cccfffe983222d6ac3ca2744c792642e74c8a
Ainsley2000 <ainsly.dsouza@students.iiit.ac.in>
2021-03-15 16:06:49 +0300

    add AWGN noise transform

--- src/dataset.py
+++ src/dataset.py
@@ -5,7 +5,19 @@ import h5py
 from utils import Kernels
 from torchvision import transforms
 import math
+import random
 
+
+class AddGaussianNoise(object):
+    def __init__(self, mean=0., std=float(random.randint(76)):
+        self.std = std
+        self.mean = mean
+        
+    def __call__(self, tensor):
+        return tensor + torch.randn(tensor.size()) * self.std + self.mean
+    
+    def __repr__(self):
+        return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
 
 class DIV2K_train(data.Dataset):
     def __init__(self, config=None):
@@ -40,12 +52,13 @@ class DIV2K_train(data.Dataset):
 
         kernel, degradinfo = next(self.kernels)
         # input (low-resolution image)
-        #TO DO: Add AWGN noise
+
         transform = transforms.Compose([
                             transforms.Lambda(lambda x: self.kernels.Blur(x,kernel)),
                             transforms.Resize((self.image_size, self.image_size),interpolation=InterpolationMode.BICUBIC),
                             transforms.Lambda(lambda x: Scaling(x)),
                             transforms.Lambda(lambda x: self.kernels.ConcatDegraInfo(x,degradinfo))
+                            AddGaussianNoise()
                     ])
         lr_image = transform(hr_image)
 
