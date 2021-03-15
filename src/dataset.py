29993b857a0f5f26cc7124b4bfd8b5ffde7b545a
bluepokeboy <>
2021-03-15 21:47:41 +0530

    Spelling

--- src/dataset.py
+++ src/dataset.py
@@ -16,10 +16,10 @@ class AddGaussianNoise(object):
     def __init__(self, mean=0., std=float(random.randint(0, 76))):
         self.std = std
         self.mean = mean
-        
+
     def __call__(self, tensor):
         return tensor + torch.randn(tensor.size()) * self.std + self.mean
-    
+
     def __repr__(self):
         return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
 
@@ -32,21 +32,21 @@ class DIV2K_train(data.Dataset):
             name = "0" * (4-int(math.log10(i))+1) + str(i)
             Y_path = config.y_path + name + '.png'
             self.image_paths.append(Y_path)
-        
+
         self.scale_factor = config.scale_factor
         self.image_size = config.image_size
-        
+
         self.kernels = Kernels(self.scale_factor)
 
 
-    def __getiterm__(self, index):
+    def __getitem__(self, index):
         Y_path = self.image_paths[index]
 
         Y_image = Image.open(Y_path).convert('RGB') # hr image
         X_image, Y_image = self.transformlr(Y_image)
 
         return X_image.to(torch.float64), Y_image.to(torch.float64)
-    
+
     def __len__(self):
         return len(self.image_paths)
 
@@ -69,5 +69,3 @@ class DIV2K_train(data.Dataset):
         transform = transforms.ToTensor()
         lr_image, hr_image = transform(lr_image), transform(hr_image)
         return lr_image, hr_image
-
-
